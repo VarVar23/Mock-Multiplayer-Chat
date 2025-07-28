5caf60991ae04969037397fc6829e38e3c4f9952
VarVar23 <oleg.nakhaev@mail.ru>
2025-07-29 00:34:18 +0300

    Create project
    Added a template from a task, connected the "System.Reactive" and "Moq" libraries, configured the Assembly Definition, and connected the Tests

---
+++ Assets/Code/ChatSystem/IChatNetwork.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Reactive.Subjects;
+using System.Threading.Tasks;
+
+namespace ChatSystem.Core
+{
+    public interface IChatNetwork
+    {
+        Task SendMessageAsync(string message, string sender); // Отправить сообщение, отправитель
+        Task RaiseEventAsync(EventType eventType, object data); // Уведомление
+        ISubject<string> OnMessageReceived { get; } // Получено сообщение
+        ISubject<(EventType, object)> OnEventReceived { get; } // Получено уведомление
+        void SimulateDisconnect(); // Симуляция отключения
+        void SimulateReconnect(); // Симуляция повторного подключения
+    }
+
+    public enum EventType
+    {
+        MatchStart, // Начало матча
+        KillNotification // Уведомление об убийстве
+    }
+}
